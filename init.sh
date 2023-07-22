9c420b0d91662eee584e6efafa0840fa5a9db828
adars <adars.nepal@gmail.com>
2023-07-22 22:52:18 +0100

    added script to make a copy of .env.example if .env does not exist

--- init.sh
+++ init.sh
@@ -1,4 +1,7 @@
-cd /var/www/html/Major_backend
+cd /var/www/Major_backend
+if [ ! -f .env ]; then
+    cp .env.example .env
+fi
 chown -R www-data:www-data storage
 php artisan key:generate
 php artisan migrate
