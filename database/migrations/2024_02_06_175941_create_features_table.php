844cb0ab6293f063081d13244d563ba6f7a4cd64
abhaya <abhaya.punkid@gmail.com>
2024-02-07 00:03:42 +0545

    added migration to create features table

---
+++ database/migrations/2024_02_06_175941_create_features_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('features', function (Blueprint $table) {
+            $table->float('bmi');
+            $table->float('weight');
+            $table->float('height');
+            $table->float('pressure');
+            $table->unsignedBigInteger("uid");
+            $table->foreign('uid')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('features');
+    }
+};
